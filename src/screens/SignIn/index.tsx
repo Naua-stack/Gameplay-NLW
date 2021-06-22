ae9b330f800ea7604a7bf65e00d2b5d366796d53
xnauax <62560321+xnauax@users.noreply.github.com>
2021-06-21 22:01:24 -0300

    Primeira aula finalizada

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,15 +1,30 @@
-import React, { useState } from "react";
+import React from "react";
 
-import { View, Text, TextInput } from "react-native";
+import { View, Text, Image, StatusBar } from "react-native";
 import { styles } from "./styles";
+import IllustrationImg from '../../assets/illustration.png';
+import {ButtonIcon} from '../../components/ButtonIcon'
 export function SignIn() {
-  const [text, setText] = useState("");
   return (
     <View style={styles.container}>
-      <Text>Hello world, NLW Together</Text>
-
-      <TextInput style={styles.input} onChangeText={setText} />
-      <Text>Você digitou: {text}</Text>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Image source={IllustrationImg} style={styles.image} resizeMode="stretch"/>
+      <View style={styles.content}>
+         <Text style={styles.title}>
+           Organize {'\n'}
+           suas jogatinas{'\n'}
+           facilmente
+        </Text>
+        <Text style={styles.subtitle}>
+         Crie grupos para jogar seus games {'\n'}
+         favoritos com seus amigos
+        </Text>
+        <ButtonIcon title={'Entrar com Discord'} activeOpacity={0.7}/>
+      </View>
     </View>
   );
 }
