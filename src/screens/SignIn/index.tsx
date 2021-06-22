829e4b7c6a3256be5b8c97945a0192b5827d59a4
Nauã  Fernandes <naua@emasoftware.com.br>
2021-06-22 13:13:10 -0300

    Aula 2

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,29 +1,28 @@
 import React from "react";
 
-import { View, Text, Image, StatusBar } from "react-native";
+import { View, Text, Image } from "react-native";
 import { styles } from "./styles";
-import IllustrationImg from '../../assets/illustration.png';
-import {ButtonIcon} from '../../components/ButtonIcon'
+import IllustrationImg from "../../assets/illustration.png";
+import { ButtonIcon } from "../../components/ButtonIcon";
 export function SignIn() {
   return (
     <View style={styles.container}>
-      <StatusBar
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
+      <Image
+        source={IllustrationImg}
+        style={styles.image}
+        resizeMode="stretch"
       />
-      <Image source={IllustrationImg} style={styles.image} resizeMode="stretch"/>
       <View style={styles.content}>
-         <Text style={styles.title}>
-           Organize {'\n'}
-           suas jogatinas{'\n'}
-           facilmente
+        <Text style={styles.title}>
+          Organize {"\n"}
+          suas jogatinas{"\n"}
+          facilmente
         </Text>
         <Text style={styles.subtitle}>
-         Crie grupos para jogar seus games {'\n'}
-         favoritos com seus amigos
+          Crie grupos para jogar seus games {"\n"}
+          favoritos com seus amigos
         </Text>
-        <ButtonIcon title={'Entrar com Discord'} activeOpacity={0.7}/>
+        <ButtonIcon title={"Entrar com Discord"} activeOpacity={0.7} />
       </View>
     </View>
   );
